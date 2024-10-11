9b719db8929abc6f902cee4a7e50e6a629f02515
Juan Pablo Leon Montenegro <juanpablo.leonm15@gmail.com>
2024-10-11 17:34:07 -0500

    Programa sin validaciones

---
+++ src/Main.java
@@ -0,0 +1,74 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int opcion, n;
+
+        do {
+            System.out.println("Menu Principal");
+            System.out.println("1. Ingrese un arreglo");
+            System.out.println("2. Ordenar Arreglo");
+            System.out.println("0. Salir");
+            opcion = scanner.nextInt();
+
+            if (opcion == 1) {
+                System.out.println("Ingrese el tamaño de su arreglo:");
+                n = scanner.nextInt();
+                int[] arregloOriginal = new int[n];
+
+                for (int i = 0; i < n; i++) {
+                    System.out.println("Ingrese el valor para " + (i + 1) + ":");
+                    arregloOriginal[i] = scanner.nextInt();
+                }
+
+                while (opcion != 0) {
+                    System.out.println("\nEscoja un método de ordenamiento:");
+                    System.out.println("1. Metodo Burbuja");
+                    System.out.println("2. Metodo Seleccion");
+                    System.out.println("3. Metodo Insercion");
+                    System.out.println("4. Metodo Burbuja Mejorado");
+                    System.out.println("0. Regresar al menu principal");
+                    opcion = scanner.nextInt();
+
+                    if (opcion != 0) {
+                        
+                        int[] arreglo = Arrays.copyOf(arregloOriginal, arregloOriginal.length);
+
+                        System.out.println("Escoja ascendente o descendente:");
+                        System.out.println("1. Ascendente");
+                        System.out.println("2. Descendente");
+                        boolean ascendente = scanner.nextInt() == 1;
+
+                        System.out.println("¿Quiere ver cada paso?");
+                        System.out.println("1. Sí");
+                        System.out.println("2. No");
+                        boolean verPasos = scanner.nextInt() == 1;
+
+                        switch (opcion) {
+                            case 1:
+                                MetodosOrdenamiento.burbuja(arreglo, ascendente, verPasos);
+                                break;
+                            case 2:
+                                MetodosOrdenamiento.seleccion(arreglo, ascendente, verPasos);
+                                break;
+                            case 3:
+                                MetodosOrdenamiento.insercion(arreglo, ascendente, verPasos);
+                                break;
+                            case 4:
+                                MetodosOrdenamiento.burbujaMejorado(arreglo, ascendente, verPasos);
+                                break;
+                        }
+
+                        System.out.println("Arreglo ordenado:");
+                        MetodosOrdenamiento.imprimirArray(arreglo);
+                    }
+                }
+            }
+
+        } while (opcion != 0);
+        scanner.close();
+    }
+}
---
+++ src/MetodosOrdenamiento.java
@@ -0,0 +1,113 @@
+import java.util.Arrays;
+
+public class MetodosOrdenamiento {
+
+    // Método Burbuja
+    public static void burbuja(int[] arr, boolean ascendente, boolean logs) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (logs) {
+                    System.out.println("i=" + i + " j=" + j + " [i]=" + arr[i] + " [j]=" + arr[j]);
+                }
+                if ((ascendente && arr[j] > arr[j + 1]) || (!ascendente && arr[j] < arr[j + 1])) {
+                    if (logs) {
+                        System.out.println("\tIntercambiamos " + arr[j] + " con " + arr[j + 1]);
+                    }
+                    
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    if (logs) {
+                        System.out.println("\t--------" + Arrays.toString(arr));
+                    }
+                }
+            }
+        }
+    }
+
+    // Método Selección
+    public static void seleccion(int[] arr, boolean ascendente, boolean logs) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minMax = i;
+            for (int j = i + 1; j < n; j++) {
+                if (logs) {
+                    System.out.println("i=" + i + " j=" + j + " [minMax]=" + arr[minMax] + " [j]=" + arr[j]);
+                }
+                if ((ascendente && arr[j] < arr[minMax]) || (!ascendente && arr[j] > arr[minMax])) {
+                    minMax = j;
+                }
+            }
+            if (logs) {
+                System.out.println("\tIntercambiamos " + arr[i] + " con " + arr[minMax]);
+            }
+            
+            int temp = arr[minMax];
+            arr[minMax] = arr[i];
+            arr[i] = temp;
+            if (logs) {
+                System.out.println("\t--------" + Arrays.toString(arr));
+            }
+        }
+    }
+
+    // Método Inserción
+    public static void insercion(int[] arr, boolean ascendente, boolean logs) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            if (logs) {
+                System.out.println("i=" + i + " j=" + j + " [key]=" + key);
+            }
+            while (j >= 0 && ((ascendente && arr[j] > key) || (!ascendente && arr[j] < key))) {
+                if (logs) {
+                    System.out.println("\tComparando " + key + " con " + arr[j]);
+                }
+                arr[j + 1] = arr[j];
+                j--;
+                if (logs) {
+                    System.out.println("\t--------" + Arrays.toString(arr));
+                }
+            }
+            arr[j + 1] = key;
+            if (logs) {
+                System.out.println("\tInsertado " + key + " en la posición " + (j + 1));
+                System.out.println("\t--------" + Arrays.toString(arr));
+            }
+        }
+    }
+
+    // Método Burbuja Mejorado
+    public static void burbujaMejorado(int[] arr, boolean ascendente, boolean logs) {
+        int n = arr.length;
+        boolean swapped;
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (logs) {
+                    System.out.println("i=" + i + " j=" + j + " [i]=" + arr[i] + " [j]=" + arr[j]);
+                }
+                if ((ascendente && arr[j] > arr[j + 1]) || (!ascendente && arr[j] < arr[j + 1])) {
+                    if (logs) {
+                        System.out.println("\tIntercambiamos " + arr[j] + " con " + arr[j + 1]);
+                    }
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                    if (logs) {
+                        System.out.println("\t--------" + Arrays.toString(arr));
+                    }
+                }
+            }
+            if (!swapped) break;  
+        }
+    }
+
+    
+    public static void imprimirArray(int[] arr) {
+        System.out.println(Arrays.toString(arr));
+    }
+}
